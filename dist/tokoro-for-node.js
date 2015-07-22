c8eb854faf7bf0b95c894ffcdb9f82cf48159727
Tsutomu Kawamura <cognitom@gmail.com>
2015-07-22 17:42:29 +0900

    コンパイル済みのスクリプト

--- dist/tokoro-for-node.js
+++ dist/tokoro-for-node.js
@@ -14,13 +14,17 @@
   path = require('path');
 
   tokoro = function(address, callback) {
-    var basePath, digest, group, key, stream;
+    var basePath, dataPath, digest, group, key, stream;
     basePath = path.join(__dirname, '..', 'data');
     key = normalize(address);
     digest = base94.encode(djb(key));
     group = Math.floor(djb(key).toString().slice(-4) / 5);
-    stream = new LineByLine(path.join(basePath, group + ".data"));
-    return stream.on('line', function(line) {
+    dataPath = path.join(basePath, group + ".data");
+    stream = new LineByLine(dataPath);
+    return stream.on('error', function(err) {
+      console.log('Data file not found.');
+      return callback('');
+    }).on('line', function(line) {
       var dg, lg, lt, ref;
       ref = line.split(' '), dg = ref[0], lt = ref[1], lg = ref[2];
       if (digest === dg) {
@@ -28,7 +32,6 @@
         return callback([base94.decode(lt) / 1000000, base94.decode(lg) / 1000000]);
       }
     }).on('end', function() {
-      console.log('Not found!');
       return callback('');
     });
   };
